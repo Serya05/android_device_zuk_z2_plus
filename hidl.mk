521b622c1c52a4bd2d126d3ab81ed030c1022dd6
MRTHAKER <mahek.haker@gmail.com>
2017-10-01 21:44:54 +0000

    z2_plus: Add missing drm hal

--- hidl.mk
+++ hidl.mk
@@ -45,7 +45,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Dumpstate HAL
 PRODUCT_PACKAGES += \
